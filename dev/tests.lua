2cc77b781303f7df8dea818eb95e980504c9b852
Tony Lagnada <deloyz@gmail.com>
2026-08-19 17:09:00 -0700

    Add proprietary LICENSE, fix Kapresoft-LibUtil Modules path packaging
    - Add LICENSE (all-rights-reserved) replacing prior MIT text
    - Update ThirdParty.xml script paths to Kapresoft-LibUtil's new
      Lib-2-0/Modules subfolder layout (upstream breaking change)
    - Sync pkgmeta.yaml and dev/setup.yml ignore lists to exclude the
      unused Kapresoft-LibUtil modules so dev/_setup builds match the
      real release package
    - Minor cleanup: release-drafter.yml whitespace, dev/tests.lua
      /run snippet updates

--- dev/tests.lua
+++ dev/tests.lua
@@ -2,13 +2,26 @@ local x = {
   -- manual load
   function()
     -- /dump C_AddOns.EnableAddOn('DebugChatFrame', UnitName('player'))
-    -- /dump EnableAddOn('DebugChatFrame', UnitName('player'))
     -- /dump C_AddOns.LoadAddOn('DebugChatFrame')
     -- /run
-    (function()
-      C_AddOns.LoadAddOn('DebugChatFrame')
-      local c = DebugChatFrame; print("DebugChatFrame=", c, 'info=', c:GetAddonInfo())
-    end)()
+      (function()
+        DCF = 'DebugChatFrame'; CA=C_AddOns EA = CA.EnableAddOn
+        EA(DCF, UnitName('player'))
+        C_Timer.After(0.5, function()
+          CA.LoadAddOn(DCF); print(DCF, _G[DCF])
+        end)
+      end)()
+    -- /dump DebugChatFrame
+    -- Setup
+    -- /run
+     (function()
+       DCF = 'DebugChatFrame'; CA=C_AddOns EA = CA.EnableAddOn
+       function _ver() local c = _G[DCF]; print(DCF, 'Loaded:', c and c:GetAddonInfo()) end
+       function _load() CA.LoadAddOn(DCF) C_Timer.After(0.5, _ver) end
+     end)()
+    -- /run
+    (function() EA(DCF, UnitName('player')); _load()
+     end)()
   end,
   -- PreReq: Temporarily set LoadOnDemand: 0
   function()
